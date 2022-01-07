03d75d16df7618a32d9fdab669728c770a230bcc
MASATAKA-FUKAYA <masataka.f@fukayashoukounoMacBook-Air.local>
2022-01-07 19:59:34 +0900

    add instance of Wizard

--- top.php
+++ top.php
@@ -7,7 +7,7 @@ session_start();
 $_SESSION = array();
 
 error_log('topページ');
-error_log(print_r($_SESSION,true));
+
 //POST送信されていた場合
 if(!empty($_POST)){
     error_log('POSTされました。');
@@ -16,17 +16,19 @@ if(!empty($_POST)){
     switch($humanFlg){
         case 'man':
             $_SESSION['human'] = new Human('男勇者', Sex::MAN, 500, 40, 120);
-            error_log('man');
             break;
         case 'woman':
             $_SESSION['human'] = new Human('女勇者', Sex::WOMAN, 500, 40, 120);
-            error_log('woman');
             break;
         case 'okama':
             $_SESSION['human'] = new Human('オカマ勇者', Sex::OKAMA, 500, 40, 120);
-            error_log('okama');
-            break;    
-
+            break;
+        case 'wizard-man':
+            $_SESSION['human'] = new Wizard('男魔法使い', Sex::MAN, 500, 30, 100);
+            break;        
+        case 'wizard-woman':
+            $_SESSION['human'] = new Wizard('女魔法使い', Sex::MAN, 500, 30, 100);
+            break; 
     }
     
     header("Location:index.php");
@@ -47,10 +49,19 @@ if(!empty($_POST)){
         <form method="post">
             <input type="radio" id="man" name="human" value="man">
             <label for="man">男勇者</label>
-            <input type="radio" id="woman" name="human" value="woman" checked>
+
+            <input type="radio" id="woman" name="human" value="woman">
             <label for="woman">女勇者</label>
+
             <input type="radio" id="okama" name="human" value="okama">
             <label for="okama">オカマ勇者</label>
+
+            <input type="radio" id="wizard-man" name="human" value="wizard-man">
+            <label for="wizard-man">男魔法使い</label>
+
+            <input type="radio" id="wizard-woman" name="human" value="wizard-woman">
+            <label for="wizard-woman">女魔法使い</label>
+
             <input type="submit" value="▶ゲームスタート">
        </form>
     </div>

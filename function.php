990d4d94b4fe35d2e5a17a674d0a47a8b590a4b2
MASATAKA-FUKAYA <masataka.f@fukayashoukounoMacBook-Air.local>
2022-01-08 19:43:58 +0900

    edit createMonster() to create BossMonster

--- function.php
+++ function.php
@@ -5,8 +5,15 @@ ini_set('error_log', 'php.log');
 
 function createMonster(){
     global $monsters;
-    $monster = $monsters[mt_rand(0,9)];
-    History::set($monster->getName().'が現れた!');
+    //５体目までモンスターを倒すと、以降はランダムでボス出現
+    if($_SESSION['knockDownCount'] >= 5){
+        $monster = $monsters[mt_rand(0,10)];
+        History::set($monster->getName().'が現れた!');
+    }else{
+        $monster = $monsters[mt_rand(0,9)];
+        History::set($monster->getName().'が現れた!!!!!!!!!');
+    }
+    
     $_SESSION['monster'] = $monster;
 }
 function init(){
